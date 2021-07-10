b80ecb671e56f56a474abaf02b6c7b119db51428
Jose Manuel Gonzalez <33811865+Josestadele@users.noreply.github.com>
2021-07-09 22:25:06 -0400

    /home

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -34,15 +34,15 @@ function App() {
                 <Home />
               </Route>
 
-              <Route path="/AboutMe">
+              <Route path="https://josestadele.github.io/portafolioweb//AboutMe">
                <AboutMe />
               </Route>
 
-              <Route path="/skills">
+              <Route path="https://josestadele.github.io/portafolioweb//skills">
                <Skills />
               </Route>
 
-              <Route path="/contact">
+              <Route path="https://josestadele.github.io/portafolioweb//contact">
                
               </Route>
 
